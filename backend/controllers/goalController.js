b602b1edc1a84b425a0c43b8499cb751659a022c
Victor Borsatto <victor.borsattobh@gmail.com>
2022-03-31 15:04:12 -0300

    JWT Authentication

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find()
+  const goals = await Goal.find({ user: req.user.id })
 
   res.status(200).json(goals)
 })
@@ -22,7 +23,8 @@ const setGoal = asyncHandler(async (req, res) => {
 
   const goal = await Goal.create(
     {
-      text: req.body.text
+      text: req.body.text,
+      user: req.user.id
     }
   )
 
@@ -40,6 +42,18 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error('Goal not found')
   }
 
+  const user = await User.findById(req.user.id)
+
+  if(!user) {
+    res.status(404)
+    throw new Error('User not found')
+  }
+
+  if(goal.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not authorized')
+  }
+
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
 
   res.status(200).json(updatedGoal)
@@ -54,6 +68,18 @@ const deleteGoal = asyncHandler(async (req, res) => {
   if(!goal) {
     res.status(404)
     throw new Error('Goal not found')
+  }
+
+  const user = await User.findById(req.user.id)
+
+  if(!user) {
+    res.status(404)
+    throw new Error('User not found')
+  }
+
+  if(goal.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not authorized')
   }
 
   await goal.remove()
--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const goalSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: [true, 'User is required'],
+      ref: 'User'
+    },
     text: {
       type: String,
       required: [true, 'Please add some text']
