e58f98a67396c82272bc042820b2911124d755be
Victor Borsatto <victor.borsattobh@gmail.com>
2022-04-01 16:54:45 -0300

    Fixing the heroku-postbuild script command

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 
@@ -21,12 +19,12 @@ app.use('/api/users', require('./routes/userRoutes'))
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) => {
+  app.get('*', (_req, res) => {
     res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
   })
 } else {
-  app.get('/', (req, res) => {
-    res.send('Server is running in production mode')
+  app.get('/', (_req, res) => {
+    res.send('Server is not running in production mode')
   })
 }
 
